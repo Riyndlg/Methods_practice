788bf29d081de8efa0da8795720eb2f923d11b07
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-05-12 10:28:42 +0300

    c

---
+++ Math_operations/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y;
+        }
+    }
+}
