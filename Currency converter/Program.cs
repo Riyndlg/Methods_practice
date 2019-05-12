5bb9103c2da1df5b7b1643ddf05df017ffd1fccd
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-05-12 12:27:35 +0300

    n

---
+++ Currency converter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Currency_converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter sum of currency to convert:");
+            double currency = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter converter value:");
+            double converter = double.Parse(Console.ReadLine());
+            double result = currency * converter;
+            Console.WriteLine($"Result: {result}");
+            Console.ReadKey();
+        }
+    }
+}
