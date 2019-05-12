c0e712a0b92ea11332da317640eb65ffe2269727
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-05-12 13:12:50 +0300

    n

---
+++ Credit payment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Credit_payment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter sum of credit:");
+            int debt = int.Parse(Console.ReadLine());
+
+            Console.ReadKey();
+        }
+        static void Payment(int payment)
+        {
+
+        }
+    }
+}
---
+++ PIN_code/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIN_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter PIN (4 or 6 digits):");
+            string pin = Console.ReadLine();
+            ValidatePin(pin);
+            Console.ReadKey();
+        }
+        static void ValidatePin(string pin)
+        {
+            if (((pin.Length == 4) || (pin.Length == 6)) && pin.Any(char.IsLetter))
+            {
+                Console.WriteLine("True");
+            }
+            else Console.WriteLine("False");
+        }
+    }
+}
