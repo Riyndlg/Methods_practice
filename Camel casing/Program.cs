1bb57a6b49ca6ad8d14b2408066b8d75a5f50b0b
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-05-13 19:59:25 +0300

    xc

--- Camel casing/Program.cs
+++ Camel casing/Program.cs
@@ -10,6 +10,21 @@ namespace Camel_casing
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter text with words delimited by dash/undercore symbols:");
+            string text = Console.ReadLine();
+            char[] separator = { '-', '_' };
+            string[] result = text.Split(separator);
+            for (int i = 1; i < result.Length; i++)
+            {
+                if (result[i].Length > 1)
+                {
+                    result[i] = result[i].Substring(0, 1).ToUpper() + result[i].Substring(1, result[i].Length - 1);
+                }
+                else result[i].ToUpper();
+            }
+            text = string.Join("", result);
+            Console.WriteLine(text);
+            Console.ReadKey();
         }
     }
 }
