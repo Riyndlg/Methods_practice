662f960221a0d975c322441300f3f5e368be960c
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-05-13 18:41:35 +0300

    c

--- Triangle_odd_numbers/Program.cs
+++ Triangle_odd_numbers/Program.cs
@@ -10,6 +10,21 @@ namespace Triangle_odd_numbers
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter row index: ");
+            int index = int.Parse(Console.ReadLine());
+            int firstNumber = 1;
+            int sum = 0;
+            for (int i = 2; i <= index * 2 - 2; i = i + 2)
+            {
+                firstNumber = firstNumber + i;
+            }
+            for (int i = 0; i < index; i++)
+            {
+                sum = sum + firstNumber;
+                firstNumber = firstNumber + 2;
+            }
+            Console.WriteLine($"Row sum for index {index}: {sum}");
+            Console.ReadKey();
         }
     }
 }
