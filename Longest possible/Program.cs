f0068365b6263f57dfe1d0f0edac1bbd69af6f5b
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-05-13 18:59:01 +0300

    n

--- Longest possible/Program.cs
+++ Longest possible/Program.cs
@@ -10,6 +10,34 @@ namespace Longest_possible
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter 2 string including only letters from a to z:");
+            string s1 = Console.ReadLine();
+            string s2 = Console.ReadLine();
+            string text = String.Concat(s1, s2);
+            string result = DuplicateSymbols(text);
+            Console.WriteLine($"Longest(s1, s2) -> {result}");
+            Console.ReadKey();
+        }
+        static string DuplicateSymbols(string text)
+        {
+            int count = 0;
+            string result = "";
+            foreach (char c in text)
+            {
+                count = 0;
+                foreach (char c2 in result) //проверка, что символ не является дубликатом
+                {
+                    if (c2 == c)
+                    {
+                        count++;
+                    }
+                }
+                if (count < 1) //проверка, что символ не повторялся еще в строке, вместо 1 может быть другое допустимое число дубликатов символа в строке
+                {
+                    result = result + c;
+                }
+            }
+            return result;
         }
     }
 }
